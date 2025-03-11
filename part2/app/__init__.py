774516e448b990b9d711350906aa8dad964157d6
adolfo2231 <adolfo.rodriguez2231@gmail.com>
2025-03-11 10:44:28 -0400

    Refactor: Improve Flask factory with config and API

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -5,14 +5,30 @@ from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.amenities import api as amenities_ns
 
-def create_app():
+
+def create_app(config_class="config.DevelopmentConfig"):
+    """
+    Flask Application Factory.
+
+    Args:
+        config_class (str): Configuration
+        class for Flask app.
+        Default is "DevelopmentConfig".
+
+    Returns:
+        Flask: Configured Flask application instance.
+    """
     app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
+    app.config.from_object(config_class)  # Load configuration 
+
+    # Create API instance with Swagger documentation
+    api = Api(app, version="1.0", title="HBnB API",
+              description="HBnB Application API")
 
     # Register all namespaces
-    api.add_namespace(users_ns, path='/api/v1/users')
-    api.add_namespace(places_ns, path='/api/v1/places')
-    api.add_namespace(reviews_ns, path='/api/v1/reviews')
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
-    
+    api.add_namespace(users_ns, path="/api/v1/users")
+    api.add_namespace(places_ns, path="/api/v1/places")
+    api.add_namespace(reviews_ns, path="/api/v1/reviews")
+    api.add_namespace(amenities_ns, path="/api/v1/amenities")
+
     return app
