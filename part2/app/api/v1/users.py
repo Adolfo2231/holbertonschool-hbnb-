382b68ef1986e0f9b8d93f20bdb73a0e17d93225
adolfo2231 <adolfo.rodriguez2231@gmail.com>
2025-03-11 12:25:27 -0400

    add password to the users endpoints

--- part2/app/api/v1/users.py
+++ part2/app/api/v1/users.py
@@ -9,7 +9,10 @@ interface for user management.
 
 from flask_restx import Namespace, Resource, fields
 from app.services import facade
-from app.models.user import User
+from werkzeug.security import generate_password_hash
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()  # Initialize Bcrypt
 
 api = Namespace('users', description='User operations')
 
@@ -19,7 +22,9 @@ user_model = api.model('User', {
                                 description='First name of the user'),
     'last_name': fields.String(required=True,
                                description='Last name of the user'),
-    'email': fields.String(required=True, description='Email of the user')
+    'email': fields.String(required=True, description='Email of the user'),
+    'password': fields.String(required=True,
+                              description='Password of the user')
 })
 
 
@@ -40,13 +45,26 @@ class UserList(Resource):
         """Register a new user."""
         user_data = api.payload
 
-        existing_user = facade.get_user_by_email(user_data['email'])
-        if existing_user:
-            return {'error': 'Email already registered'}, 400
+        try:
+            # Validate required fields
+            if not all([user_data['first_name'],
+                        user_data['last_name'],
+                       user_data['email'],
+                       user_data['password']]):
+                return {"error": "Missing required fields"}, 400
 
-        new_user = facade.create_user(user_data)
-        return {'id': new_user.id, 'first_name': new_user.first_name,
-                'last_name': new_user.last_name, 'email': new_user.email}, 201
+            existing_user = facade.get_user_by_email(user_data['email'])
+            if existing_user:
+                return {'error': 'Email already registered'}, 400
+
+            new_user = facade.create_user(user_data)
+            return {'id': new_user.id,
+                    'first_name': new_user.first_name,
+                    'last_name': new_user.last_name,
+                    'email': new_user.email}, 201
+
+        except ValueError as e:
+            return {"error": str(e)}, 400
 
 
 @api.route('/<user_id>')
